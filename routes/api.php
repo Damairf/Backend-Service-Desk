1e1235a1bac6c4fa386c2166b6d8a480a99836ab
Alfathanf <alfathanfarrel0305@gmail.com>
2025-07-20 18:44:53 +0700

    farrel commit modif route

--- routes/api.php
+++ routes/api.php
@@ -25,22 +25,22 @@ Route::post('/user',[UserController::class, 'createOne_User'])->middleware(CekTo
 Route::get('/user',[UserController::class, 'findAll_User'])->middleware(CekToken::class);
 
 // api Role
-Route::get('/role',[RoleController::class, 'findAll_Role']);
-Route::get('/role/{roleId}',[RoleController::class, 'findOne_Role']);
+Route::get('/role',[RoleController::class, 'findAll_Role'])->middleware(CekToken::class);
+Route::get('/role/{roleId}',[RoleController::class, 'findOne_Role'])->middleware(CekToken::class);
 
 // api Jabatan
-Route::get('/jabatan',[JabatanController::class, 'findAll_Jabatan']);
-Route::get('/jabatan/{jabatanId}',[JabatanController::class, 'findOne_Jabatan']);
-Route::post('/jabatan',[JabatanController::class, 'insertOne_Jabatan']); 
-Route::put('/jabatan/{jabatanId}',[JabatanController::class, 'updateOne_Jabatan']);
-Route::delete('/jabatan/{jabatanId}',[JabatanController::class, 'deleteOne_Jabatan']);
+Route::get('/jabatan',[JabatanController::class, 'findAll_Jabatan'])->middleware(CekToken::class);
+Route::get('/jabatan/{jabatanId}',[JabatanController::class, 'findOne_Jabatan'])->middleware(CekToken::class);
+Route::post('/jabatan',[JabatanController::class, 'insertOne_Jabatan'])->middleware(CekToken::class);
+Route::put('/jabatan/{jabatanId}',[JabatanController::class, 'updateOne_Jabatan'])->middleware(CekToken::class);
+Route::delete('/jabatan/{jabatanId}',[JabatanController::class, 'deleteOne_Jabatan'])->middleware(CekToken::class);
 
 // api Organisasi
-Route::get('/organisasi',[OrganisasiController::class, 'findAll_Organisasi']);
+Route::get('/organisasi',[OrganisasiController::class, 'findAll_Organisasi'])->middleware(CekToken::class);
 
 // api Status
-Route::get('/status',[StatusController::class, 'findAll_Status']);
-Route::get('/status/{statusId}',[StatusController::class, 'findOne_Status']);
+Route::get('/status',[StatusController::class, 'findAll_Status'])->middleware(CekToken::class);
+Route::get('/status/{statusId}',[StatusController::class, 'findOne_Status'])->middleware(CekToken::class);
 
 // api Permintaan
-Route::get('/permintaan',[PelayananController::class, 'getAll_Permintaan']);
+Route::get('/permintaan',[PelayananController::class, 'getAll_Permintaan'])->middleware(CekToken::class);
